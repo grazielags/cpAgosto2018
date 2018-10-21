1d601fd40572eb58221da7852c206666b727c3d9
BauerrDeveloper <ro.bauerr@gmail.com>
2018-10-21 20:58:59 -0300

    Exercicios de 01 a 03 da aula 04 de Java

--- RodrigoPaulo/Java/projetosJava/projeto01/projetoAula/src/Aula04/exercicio02.java
+++ RodrigoPaulo/Java/projetosJava/projeto01/projetoAula/src/Aula04/exercicio02.java
@@ -1,10 +1,55 @@
 package Aula04;
 
+import java.util.Scanner;
+
 public class exercicio02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		
+		int numero;
+		
+		System.out.println("Digite um número entre 0 e 10: ");
+		numero = input.nextInt();
+		
+		switch (numero) {
+		case 1:
+			System.out.println("Um");
+			break;
+		case 2:
+			System.out.println("Dois");
+			break;
+		case 3:
+			System.out.println("Três");
+			break;
+		case 4:
+			System.out.println("Quatro");
+			break;
+		case 5:
+			System.out.println("Cinco");
+			break;
+		case 6:
+			System.out.println("Seis");
+			break;
+		case 7:
+			System.out.println("Sete");
+			break;
+		case 8:
+			System.out.println("Oito");
+			break;
+		case 9:
+			System.out.println("Nove");
+			break;
+		case 10:
+			System.out.println("Dez");
+			break;
+		default:
+			System.out.println("Opção Inválida!");
+		
+		}
+		System.out.println();
+		System.out.println("Encerrando aplicação...");
 	}
 
 }
--- RodrigoPaulo/Java/projetosJava/projeto01/projetoAula/src/Aula04/exercicio03.java
+++ RodrigoPaulo/Java/projetosJava/projeto01/projetoAula/src/Aula04/exercicio03.java
@@ -1,10 +1,56 @@
 package Aula04;
 
+import java.util.Scanner;
+
 public class exercicio03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
 
+		int operacao;
+		double n1;
+		double n2;
+		double resultado;
+
+		System.out.println(" >>> Calculadora <<<");
+		System.out.println();
+
+		System.out.println("Digite o primeiro numero: ");
+		n1 = input.nextDouble();
+
+		System.out.println("Digite o segundo numero: ");
+		n2 = input.nextDouble();
+
+		System.out.println("Digite a operação que deseja realizar conforme tabela abaixo: ");
+		System.out.println("   DIGITE 1 PARA SOMA");
+		System.out.println("   DIGITE 2 PARA DIVISÃO");
+		System.out.println("   DIGITE 3 PARA SUBTRAÇÃO");
+		System.out.println("   DIGITE 4 PARA MULTIPLICAÇÃO");
+		operacao = input.nextInt();
+
+		switch (operacao) {
+		case 1:
+			resultado = (n1 + n2);
+			System.out.println("O resultado da soma de é: " + resultado);
+			break;
+		case 2:
+			resultado = (n1 / n2);
+			System.out.println("O resultado da divisão é: " + resultado);
+			break;
+		case 3:
+			resultado = (n1 - n2);
+			System.out.println("O resultado da subtração é: " + resultado);
+			break;
+		case 4:
+			resultado = (n1 * n2);
+			System.out.println("O resultado da multiplicaço é: " + resultado);
+			break;
+		default:
+			System.out.println("Opção Inválida!");
+		}
+		System.out.println();
+		System.out.println("Encerrando aplicação...");
 	}
 
 }
