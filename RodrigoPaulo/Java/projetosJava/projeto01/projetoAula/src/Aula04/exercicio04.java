bd3d4684e1cc586fa961ef84cfde2c22cf801106
BauerrDeveloper <ro.bauerr@gmail.com>
2018-10-22 14:33:51 -0300

    Exercicios aula 04 resolvidos.

--- RodrigoPaulo/Java/projetosJava/projeto01/projetoAula/src/Aula04/exercicio04.java
+++ RodrigoPaulo/Java/projetosJava/projeto01/projetoAula/src/Aula04/exercicio04.java
@@ -1,9 +1,40 @@
 package Aula04;
 
+import java.util.Scanner;
+
 public class exercicio04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		
+		int numero;
+		
+		System.out.println("Digite um número entre 0 e 5: ");
+		numero = input.nextInt();
+		
+		switch (numero) {
+		case 1:
+			System.out.println("Um");
+			break;
+		case 2:
+			System.out.println("Dois");
+			break;
+		case 3:
+			System.out.println("Três");
+			break;
+		case 4:
+			System.out.println("Quatro");
+			break;
+		case 5:
+			System.out.println("Cinco");
+			break;
+		default:
+			System.out.println("Opção Inválida!");
+		
+		}
+		System.out.println();
+		System.out.println("Encerrando aplicação...");
 
 	}
 
--- RodrigoPaulo/Java/projetosJava/projeto01/projetoAula/src/Aula04/exercicio05.java
+++ RodrigoPaulo/Java/projetosJava/projeto01/projetoAula/src/Aula04/exercicio05.java
@@ -1,10 +1,74 @@
 package Aula04;
 
+import java.util.Scanner;
+
 public class exercicio05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		
+		int numero;
+		int ano;
+		boolean bissexto;
+		
+		System.out.println("Digite um número de 1 a 12, representando o mês escolhido:");
+		numero = input.nextInt();
+		
+		System.out.println("Digite um ano ");
+		ano = input.nextInt();
+		
+		
+		switch(numero)
+		
+		{
+		    case 1:
+		    	System.out.println("Mês de janeiro possui 31 dias.");
+		            break;
+		    case 2:
+		    	bissexto = ( ( ano % 4 == 0 && ano % 100 != 0 ) || ( ano % 400 == 0 ) );
+		    	if (bissexto) { 
+		    		System.out.println("Esse ano é bissexto, o mês de fevereiro possui 29 dias.");
+		        }
+		        else{
+		        	System.out.println("Mês de fevereiro possui 28 dias.");
+		        }
+					
+		            break;
+		    case 3:
+		    	System.out.println("Mês de março possui 31 dias.");
+		            break; 
+		    case 4:
+		    	System.out.println("Mês de abril possui 30 dias.");
+		            break;    
+		    case 5:
+		    	System.out.println("Mês de maio possui 31 dias.");
+		            break;
+		    case 6:
+		    	System.out.println("Mês de junho possui 30 dias.");
+		            break;
+		    case 7:
+		    	System.out.println("Mês de julho possui 31 dias.");
+		            break;
+		    case 8:
+		    	System.out.println("Mês de agosto possui 31 dias.");
+		            break;
+		    case 9:
+		    	System.out.println("Mês de setembro possui 30 dias.");
+		            break;
+		    case 10:
+		    	System.out.println("Mês de outubro possui 31 dias.");
+		            break;
+		    case 11:
+		    	System.out.println("Mês de novembro possui 30 dias.");
+		            break;
+		    case 12:
+		    	System.out.println("Mês de dezembro possui 31 dias.");
+		            break;
+		    default:
+		    	System.out.println("Opção inválida, escolha um número de 1 a 12.");
+		}	
 
 	}
-
 }
+
