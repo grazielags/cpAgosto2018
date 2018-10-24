a093a94d365b3b6b02ea46bce8aeea3df32c87d7
pati <pati@pop-os.localdomain>
2018-10-24 20:56:23 -0300

    Atualização parcial

---
+++ patricia/java/src/Aula6/Exercicio1.java
@@ -0,0 +1,24 @@
+package Aula6;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		int altura = 0;
+		for (int i = 0; i < 4; i++) {
+			altura = JOptionPane.showInputDialog("Informe a altura");
+			altura = JOptionPane.showInputDialog("Informe a altura");
+			
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
+// Escrever um algoritmo que leia um conjunto de 5 informações contendo, cada
+// uma delas, a altura e o sexo de uma pessoa (código=1 - masculino, código=2 -
+// feminino), calcule e mostre o seguinte:
+// a) a maior e a menor altura da turma
+// b) a média da altura das mulheres
+// c) a média da altura da turma.
