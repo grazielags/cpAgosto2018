65043c8f5ae5c5f762ce853f8d56ddfaa1be1583
Graziela Göedert de Souza <graziela@gmail.com>
2018-10-29 22:02:01 -0300

    Exemplos.

---
+++ Graziela/Java/src/Aula7/Exercicio1.java
@@ -0,0 +1,26 @@
+package Aula7;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio1 {
+	public static void main(String args[]) {
+		int[][] numeros = new int[3][3];
+		boolean isMatrizIdentidade = true;
+		for (int linha = 0; linha < numeros.length; linha++) {
+			for (int coluna = 0; coluna < numeros[linha].length; coluna++) {
+				numeros[linha][coluna] = Integer.parseInt(JOptionPane.showInputDialog(
+						"Digite um número[" + linha + "][" + coluna + "]!"));
+				if(linha == coluna && numeros[linha][coluna] != 1) {
+					isMatrizIdentidade = false;
+					break;
+				}
+				if(linha != coluna && numeros[linha][coluna] != 0) {
+					isMatrizIdentidade = false;
+					break;
+				}
+			}
+		}
+		JOptionPane.showMessageDialog(null, "É uma matriz identidade? " + isMatrizIdentidade);
+	}
+}
+
